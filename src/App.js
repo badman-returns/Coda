4cd0c3c44dc9cad9190066120042da52ba036cf5
tsgoswami <trishnangshugoswami@yahoo.in>
2021-09-24 00:11:29 +0530

    add: login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import {  makeStyles } from '@material-ui/core';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+      minHeight: '100vh',
+      width: '100%',
+      backgroundColor: '#fff',
+      // backgroundImage: `url(${process.env.PUBLIC_URL + 'background.jpg'})`,
+      backgroundImage: 'linear-gradient(360deg, rgba(244,241,246,1) 4%, rgba(203,29,164,1) 85%)',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      zIndex: -1,
+      opacity: 0.8
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/">
+          <div className={classes.root}>
+            <Login />
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+    CssBaseline,
+    Grid,
+    makeStyles,
+    Button,
+    Card,
+    Typography
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '80vh',
+        fontFamily: 'Nunito',
+    },
+    appbarTitle: {
+        flexGrow: '1',
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    card: {
+        backgroundColor: 'transparent',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    button: {
+        borderRadius: '10px',
+        textDecoration: 'none',
+        color: '#000',
+        width: '300px',
+        height: '80px',
+        fontWeight: '800',
+    },
+    buttonImg: {
+        width: '20%',
+        margin: '0 auto',
+    },
+    quotation: {
+        color: '#fff',
+        textTransform: 'uppercase',
+        margin: '20px 0'
+    },
+    subtitle: {
+        color: '#fff',
+    }
+
+}));
+
+function Login() {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+                <div className={classes.paper}>
+                    <Card className={classes.card} elevation={0}>
+                        <Grid container direction='column' justifyContent="center" alignItems="center">
+                            <img src={`${process.env.PUBLIC_URL + 'logo.png'}`} alt="logo" />
+                            <Button variant="contained" className={classes.button}>
+                                <Grid container alignItems="center" spacing={2}>
+                                    <img className={classes.buttonImg} src={`${process.env.PUBLIC_URL + 'spotify.png'}`} alt="spotify" />
+                                    <Typography variant="h6" >Login with Spotify</Typography>
+                                </Grid>
+                            </Button>
+                            <Typography variant="subtitle1" className={classes.subtitle}>Made withn 💖 by T S Goswami</Typography>
+                        </Grid>
+                    </Card>
+                </div>
+        </div >
+    )
+}
+
+export default Login
